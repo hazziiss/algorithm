7ba9b5aeba554df769dcc7a8b9d604745be709e9
HYEJI LEE <98932039+hazziiss@users.noreply.github.com>
2023-01-16 13:55:28 +0900

    [Bronze IV] Title: 주사위 세개, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,14 +1,10 @@
 a, b, c = map(int, input().split())
-li = [a, b, c]
 
 if a == b == c :
-    print(10000 + a * 1000)
-
-elif a == b or a == c:
-    print(1000 + a * 100) 
-
+    print(10000+a*1000)
+elif a == b or a == c :
+    print(1000+a*100)
 elif b == c :
-    print(1000 + b * 100)
-
+    print(1000+b*100)  
 else :
-    print (max(li)*100)
+    print(max(a, b, c)*100)          
