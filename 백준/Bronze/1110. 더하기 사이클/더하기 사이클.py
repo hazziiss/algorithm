c6fbc76227e97860e701e380fd2c503a162fe6ec
HYEJI LEE <98932039+hazziiss@users.noreply.github.com>
2023-01-17 14:29:02 +0900

    [Bronze I] Title: 더하기 사이클, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -1,14 +1,15 @@
-n = int(input())
-new = n
+n = int(input()) #26
+new = n #26
 cnt = 0
 
-while True :      
-    one = new % 10
-    ten = new // 10
-
-    new = 10 * one + (one + ten) % 10
-
+while True :
+    one = new % 10 #6
+    ten = new // 10 #2
+    new = one*10 + (one+ten)%10 #68
+    # print(new)
     cnt += 1
-    if (n == new) :
+
+    if n == new :
         break
+
 print(cnt)  
