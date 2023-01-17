01853fcca73994b377e3b04b1ef80ba1ee76849b
HYEJI LEE <98932039+hazziiss@users.noreply.github.com>
2023-01-17 13:34:06 +0900

    [Bronze III] Title: 최댓값, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -4,11 +4,10 @@ cnt = 1
 for i in range(9) :
     a = int(input())
     li.append(a)
-
 for i in li :
     if i != max(li) :
         cnt += 1
-    else : 
-        break
-print(max(li)) 
-print(cnt) 
+    else :
+        break        
+    
+print(max(li), cnt, sep="\n")
